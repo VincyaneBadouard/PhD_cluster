8bb9f325eda6b49e8d422a1d58f72628f5c1aa33
Vincyane BADOUARD <badouard@cirad.fr>
2025-05-02 17:57:23 +0200

    Balance data and null model

--- Topo_Light_Onto/Autocor_explo/Model/Null.stan
+++ Topo_Light_Onto/Autocor_explo/Model/Null.stan
@@ -5,9 +5,13 @@ data {
   array[N] int<lower=0, upper=1> Presence ; // Response
 }
 parameters {
-  real theta;
+  real theta ;
 }
 model {
-  Presence ~ bernoulli_logit(theta); // no expl variables
+  Presence ~ bernoulli_logit(theta) ; // no expl variables
+}
+generated quantities { // predictions
+vector<lower=0, upper=1>[N] p ;
+p = rep_vector(inv_logit(theta), N) ; // replicate the real
 }
 
--- Topo_Light_Onto/Autocor_explo/Sampling_balance.R
+++ Topo_Light_Onto/Autocor_explo/Sampling_balance.R
@@ -3,8 +3,21 @@
 library(tidyverse)
 library(cmdstanr)
 
+# Args
+arg <- commandArgs(trailingOnly = TRUE)
+ID <- as.integer(arg[1])
+# ID = 1
+iter <- as.integer(arg[2])
+# iter = 2000
+
 # Species of interest
-s <- "Dicorynia_guianensis"
+# sp <- (read.csv("../../Data/InterestSpecies.csv")[,2]) # 75
+sp <- c("Anaxagorea_dolichocarpa", "Tabernaemontana_macrocalyx", # 5 agreg sp
+        "Eperua_falcata", "Dicorynia_guianensis", "Paypayrola_hulkiana")
+print(paste("ID:", ID))
+s <- sp[ID]
+
+print(paste("run for sp", s))
 
 setwd("D:/Mes Donnees/PhD/R_codes/PhD_cluster/Topo_Light_Onto/Autocor_explo/")
 if(!file.exists("Chains"))
@@ -27,7 +40,6 @@ dataM <- list(N = nrow(DATA), # 47 850
               Light = DATA$logTransmittance,
               Topography = DATA$logTWI,
               DBH = DATA$logDBH)
-getwd()
 
 # Model
 Sys.time()
@@ -42,15 +54,42 @@ Sys.time() # 1 min
 
 # Sampling
 Sys.time()
-chain_path <- file.path("Chains", model_name, s)
+chain_path <- file.path("Chains", "Hybrid_balance", s)
 if(!file.exists(chain_path)){
   # unlink(chain_path, recursive = TRUE)
   dir.create(chain_path)
   fit <- model$sample(data = dataM,
                       chains = 4,
                       parallel_chains = 4,
-                      iter_warmup = floor(2000/2),
-                      iter_sampling = 2000,
+                      iter_warmup = floor(iter/2),
+                      iter_sampling = iter,
+                      save_warmup = FALSE)
+  fit$save_output_files(dir = chain_path)
+}
+Sys.time() # 1 min
+
+# Model
+Sys.time()
+model_name <- "Null"
+model <- cmdstan_model(
+  file.path(
+    "Model",
+    paste0(model_name, ".stan")
+  ))
+Sys.time() # 1 min
+
+
+# Sampling
+Sys.time()
+chain_path <- file.path("Chains", "Null_balance", s)
+if(!file.exists(chain_path)){
+  # unlink(chain_path, recursive = TRUE)
+  dir.create(chain_path)
+  fit <- model$sample(data = dataM,
+                      chains = 4,
+                      parallel_chains = 4,
+                      iter_warmup = floor(iter/2),
+                      iter_sampling = iter,
                       save_warmup = FALSE)
   fit$save_output_files(dir = chain_path)
 }
