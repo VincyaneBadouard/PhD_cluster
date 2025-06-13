e1f668093a9c2f988055525eb78795295c9a4df6
Vincyane BADOUARD <badouard@cirad.fr>
2025-06-13 11:49:50 +0200

    Model discrete ontogeny

---
+++ Topo_Light_Onto/Sampling_sp_array_DBH_classes.R
@@ -0,0 +1,90 @@
+
+# Species
+library(tidyverse)
+library(cmdstanr)
+
+# Args
+arg <- commandArgs(trailingOnly = TRUE)
+ID <- as.integer(arg[1])
+# ID = 1
+iter <- as.integer(arg[2])
+# iter = 2000
+Np <- 50 # number of predictions
+
+Combin <- read_csv("Data/Combin_Sp_DBHclas.csv")
+
+# setwd("D:/Mes Donnees/PhD/R_codes/PhD_cluster/Topo_Light_Onto/")
+
+print(paste("ID:", ID))
+s <- Combin[ID,]$Species
+# s = "Eschweilera_coriacea"
+
+print(paste("run for sp", s, "DBH class:", Combin[ID,]$DBH_classes))
+
+if(!file.exists("Chains"))
+  dir.create("Chains")
+if(!file.exists("Chains/Hybrid"))
+  dir.create("Chains/Hybrid")
+getwd()
+
+# Presence data
+# load real presence-absences data on 25ha 
+load("../Data/Realsp_25ha.Rdata")
+# View(datalist[[1]])
+
+x <- datalist[names(datalist) %in% s][[s]] %>% # only species in sp
+  filter(DBHcor >= Combin[ID,]$Min & DBHcor < Combin[ID,]$Max) # only the DBH class
+# View(datalist[["Iryanthera_hostmannii"]])
+
+
+# DataM
+# les noms des var doivent etre les memes que dans le fichier stan
+dataM <- list(N = nrow(x), # 47 850
+              Presence = x$Presence,
+              Light = x$logTransmittance,
+              Topography = x$logTWI,
+              # number of predictions
+              N_L_p = Np, # light
+              N_T_p = Np, # topography
+              # environment of predictions
+              Lightp = seq(min(x$logTransmittance), max(x$logTransmittance), length.out = Np),
+              # Topographyp = seq(min(x$logTWI), max(x$logTWI), length.out = Np),
+              Topographyp = median(x[x$Presence==1,]$logTWI) # the topography the most represented
+)
+
+# median(datalist[[1]][datalist[[1]]$Presence==1,]$TWI)
+# hist(datalist[[1]][datalist[[1]]$Presence==1,]$TWI)
+# median(datalist[[1]][datalist[[1]]$Presence==1,]$logTWI)
+# seq(min(datalist[[1]]$logTWI), max(datalist[[1]]$logTWI), length.out = Np) # 1.358045 - 2.400176
+# seq(min(datalist[[1]]$logTransmittance), max(datalist[[1]]$logTransmittance), length.out = Np) # -7.308724e+00 , 8.941571e-08
+
+
+# Model
+Sys.time()
+model_name <- "Hybrid_no_iota"
+model <- cmdstan_model(
+  file.path(
+    "Model",
+    paste0(model_name, ".stan")
+  ))
+Sys.time() # 1 min
+
+
+# Sampling
+Sys.time()
+chain_path <- file.path("Chains", model_name, s, Combin[ID,]$DBH_classes)
+if(!file.exists(chain_path)){
+  # unlink(chain_path, recursive = TRUE)
+  dir.create(chain_path)
+  fit <- model$sample(data = dataM,
+                      chains = 4,
+                      parallel_chains = 4,
+                      iter_warmup = floor(iter/2),
+                      iter_sampling = iter,
+                      save_warmup = FALSE)
+  fit$save_output_files(dir = chain_path)
+}
+Sys.time() # 50 min
+
+print(paste(s, "DONE"))
+
