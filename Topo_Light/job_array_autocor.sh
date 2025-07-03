5b65108afe81d700779878d1f51d8fdd06078c06
Badouard Vincyane <badouardv@muse-login01.cluster>
2025-07-03 07:14:30 +0200

    without onto autocor

--- Topo_Light/job_array_autocor.sh
+++ Topo_Light/job_array_autocor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=Topo_Light_Onto_sp_array
+#SBATCH --job-name=Topo_Light_sp_array_autocor
 #SBATCH --account=agap
 #SBATCH --partition=agap_normal
-#SBATCH --output=/lustre/badouardv/PhD_cluster/Topo_Light_Onto/logsout/log_%a.out
-#SBATCH --error=/lustre/badouardv/PhD_cluster/Topo_Light_Onto/logserr/log_%a.err
-#SBATCH --array=1-40
+#SBATCH --output=/lustre/badouardv/PhD_cluster/Topo_Light/logsout/log_%a.out
+#SBATCH --error=/lustre/badouardv/PhD_cluster/Topo_Light/logserr/log_%a.err
+#SBATCH --array=1-75
 #SBATCH --time=5:00:00
 #SBATCH --ntasks-per-core=1
 #SBATCH --cpus-per-task=4
@@ -13,6 +13,6 @@
 module purge
 module load singularity/3.6.3
 
-cd /lustre/badouardv/PhD_cluster/Topo_Light_Onto/
+cd /lustre/badouardv/PhD_cluster/Topo_Light/
 singularity exec ../Singularity/cmdstanr.sif  Rscript Sampling_eigenvectors.R ${SLURM_ARRAY_TASK_ID} 2000
 
