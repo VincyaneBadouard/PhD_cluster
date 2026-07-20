525bc49627f65a0b6c6cff48a42543422c4288b6
Badouard Vincyane <badouardv@io-login-01.io.internal>
2026-07-20 16:07:16 +0200

    100 iter

--- Light_uncertainty/job_array_autocor.sh
+++ Light_uncertainty/job_array_autocor.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=Light_uncertainty
-#SBATCH --account=dedicated-cpu@cirad-normal
+#SBATCH --account=dedicated-cpu@cirad-long
 #SBATCH --partition=cpu-dedicated
 #SBATCH --output=logsout/log_%a.out
 #SBATCH --error=logserr/log_%a.err
 #SBATCH --array=1-200
-#SBATCH --time=5:00:00
+#SBATCH --time=50:00:00
 #SBATCH --ntasks-per-core=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mail-user=vincyane.badouard@gmail.com
